53b488702980de55d4e251cd2cb8e2e886dfa055
Tushar <td220627@gmail.com>
2025-06-29 19:59:28 +0530

    Requirements and Setup File Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tushar Das',
+    author_email= 'td220627@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
